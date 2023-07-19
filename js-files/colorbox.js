8f9e9d1f390b4cf87b18f4d40d067a0391816997
dprittj <dprittj@gmail.com>
2023-07-19 13:34:17 -0500

    added color flip box to jsoutput page with colorbox.js interaction

---
+++ js-files/colorbox.js
@@ -0,0 +1,21 @@
+const hex = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btnColor = document.getElementById("btnColor");
+const div3 = document.getElementById("div3");
+
+const color = document.querySelector(".colorbox");
+
+btnColor.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++){
+        hexColor +=hex[getRandomNumber()];
+    }
+
+    // color.textContent = hexColor;
+    div3.style.backgroundColor = hexColor;
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*hex.length);
+};
