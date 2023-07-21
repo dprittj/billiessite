abf7d3c9f1b308cd06476c7c4d3fa4aae9b6b807
dprittj <dprittj@gmail.com>
2023-07-20 19:30:59 -0500

    fixed stying attribution on dynamically created <p> element with additional code to helloWorldFunc.js; styling meant only for this element no longer applies to other <p> elements

--- js-files/colorbox.js
+++ js-files/colorbox.js
@@ -3,8 +3,6 @@ const hex = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btnColor = document.getElementById("btnColor");
 const div3 = document.getElementById("div3");
 
-const color = document.querySelector(".colorbox");
-
 btnColor.addEventListener('click', function(){
     let hexColor = "#";
 
@@ -12,10 +10,10 @@ btnColor.addEventListener('click', function(){
         hexColor +=hex[getRandomNumber()];
     }
 
-    // color.textContent = hexColor;
     div3.style.backgroundColor = hexColor;
 })
 
 function getRandomNumber(){
     return Math.floor(Math.random()*hex.length);
 };
+
--- js-files/helloWorldFunc.js
+++ js-files/helloWorldFunc.js
@@ -1,21 +1,27 @@
 
 const btn = document.getElementById("declaredHello");
 
+
 function declaredHello(){
-    
+    // this block creates a new <p> element and adds text to it
     const newP = document.createElement("p");
-    
     const returnHello = document.createTextNode("Hello World");
-    
     newP.appendChild(returnHello);
     
-    // const btnGreet = document.getElementById("declaredHello");
+    // the new <p> is put onto the DOM above the IDed element
     const div2 = document.getElementById("div2");
-
     document.body.insertBefore(newP, div2);
-    // document.body.insertAfter(newP, div2);
+
+    /* originally, placed immediately after the element creation above (to no avail), 
+    down here, these lines attach the class="newP" to the new <p> element */
+    const newPclass = document.createAttribute("class");
+    newPclass.value = "newP";
+    const P = document.getElementsByTagName("p")[0];
+    P.setAttributeNode(newPclass);
 }
 
+
+// on clicking the button, a greeting appears and the button disappears
 btn.addEventListener('click', function(){
 
     const div2 = document.getElementById("div2");
